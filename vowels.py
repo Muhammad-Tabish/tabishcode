36cabf887a47d92538c0103191b8c80a31354694
tabishhameed <tabishhameed34@gmail.com>
2020-12-15 16:57:20 +0800

    vowel count

--- vowels.py
+++ vowels.py
@@ -4,10 +4,17 @@ input_string = input_string.casefold()
 vowels = "aeiou"
 
 vowels_data = {}.fromkeys(vowels, 0)
+total_vowels = 0
 
 print(vowels_data)
 
 for characters in input_string:
     print(characters)
+    if characters in vowels:
+        vowels_data[characters] += 1
+        total_vowels +=1
+print("total vowels count: ", total_vowels)
+for vowel in vowels_data:
+    print(vowel, " =>", vowels_data[vowel])
 
 
